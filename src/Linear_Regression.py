5b066d3a5c1d285896e9e925673b88a4abc83085
Teo Asinari <tasinari@protonmail.com>
2022-06-16 18:44:33 -0700

    Draft of linear regression.

---
+++ src/Linear_Regression.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class LinearRegression:
+    def regression(data): 
+        w = np.ones((1, 1))
+        b = np.ones((1, 1))
+
+if __name__ == "__main__":
+    #train_inputdata = np.array(())
+    #train_outputdata = np.array(()) 
+
