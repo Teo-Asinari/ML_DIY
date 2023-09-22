d66b2b4e06f022adced0a27fac7f67e7f2ea5b42
Teo Asinari <tasinari@protonmail.com>
2023-09-22 08:52:23 -0700

    Simplified MLP code and got forward pass working

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -15,34 +15,39 @@ class MLP:
     output_layer_schema = (ActivationFunction.RELU, 5)
     layers = []
 
-    def __init__(self, input_size, input_layer_schema, hidden_layer_schemas, output_layer_schema):
+    def __init__(self, input_size, layer_schemas):
         self.input_size = input_size
-        self.input_layer_schema = input_layer_schema
-        self.hidden_layer_schemas = hidden_layer_schemas
-        self.output_layer_schema = output_layer_schema
+        self.layer_schemas = layer_schemas
 
     def initLayers(self):
         self.layers = []
-        self.layers.append(np.random.rand((self.input_layer_schema[1], self.input_size)))
+        self.layers.append(np.random.rand(self.layer_schemas[0][1], self.input_size))
 
-        prev_layer_size = self.input_layer_schema[1]
-        self.layers.append(np.random.rand((self.hidden_layer_schemas[0][1], prev_layer_size)))
-        prev_layer_size = self.hidden_layer_schemas[0][1]
+        prev_layer_size = self.layer_schemas[0][1]
 
-        for hiddenLayer in self.hidden_layer_schemas[1:]:
-            self.layers.append(np.random.rand((hiddenLayer[1], prev_layer_size)))
-            prev_layer_size = hiddenLayer[1]
+        for layer in self.layer_schemas[1:]:
+            self.layers.append(np.random.rand(layer[1], prev_layer_size))
+            prev_layer_size = layer[1]
 
-        self.layers.append(np.random.rand((self.output_layer_schema[1], prev_layer_size)))
         print("These are the layers: " + str(self.layers))
 
-#    def forwardPass(self, input_vec):
+    def forwardPass(self, input_vec):
+         currVal = input_vec
+         print("curr_val start is: " + str(currVal))
+         for layerIdx in range(len(self.layers)):
+             currActivationFunc = self.layer_schemas[layerIdx][0]
+             currActivation = np.dot(self.layers[layerIdx], currVal)
+             print("curr_val before activation func is: " + str(currActivation))
+             currVal = currActivationFunc(currActivation)
+             print("curr_val after activation func is: " + str(currVal))
+         print("Final curr_val is: " + str(currVal))
+         return currVal
 
 
 if __name__ == '__main__':
     print("Begin construct basic MLP")
-    inputLayerSchema = (ActivationFunction.LOGISTIC, 5)
-    hiddenLayerSchemas = [(ActivationFunction.TANH, 5), (ActivationFunction.TANH, 5)]
-    outputLayerSchema = (ActivationFunction.RELU, 5)
-    basicMLP = MLP(5, inputLayerSchema, hiddenLayerSchemas, outputLayerSchema)
+    layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
+                          (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
+    basicMLP = MLP(5, layerSchemas)
     basicMLP.initLayers()
+    basicMLP.forwardPass(np.array([1,2,3,4,5]))
