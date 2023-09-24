544c9fb107cdda3c93f04143d929e971bdabe483
Teo Asinari <tasinari@protonmail.com>
2023-09-24 16:11:28 -0700

    More refactoring, removing unnecessary code and switching to using packages/modules

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -2,6 +2,8 @@ import argparse
 import numpy as np
 from enum import Enum
 
+from Utils.DebugUtils import printd
+
 from typing import List, Tuple, Callable
 
 class ActivationFunction(Enum):
@@ -14,24 +16,14 @@ layerSchema = Tuple[activationFunc, int]
 
 DEBUG = False
 
-def printd(string : str) -> None:
-    if DEBUG:
-        print(string)
-
-
 class MLP:
-    input_size = 5
-    layer_schemas = []
-    layers = []
-
     def __init__(self, input_size: int, layer_schemas: List[layerSchema]):
         self.input_size = input_size
         self.layer_schemas = layer_schemas
         self.layers = self.initLayers()
 
     def initLayers(self) -> List[np.ndarray]:
-        layers = []
-        layers.append(np.random.rand(self.layer_schemas[0][1], self.input_size))
+        layers = [np.random.rand(self.layer_schemas[0][1], self.input_size)]
 
         prev_layer_size = self.layer_schemas[0][1]
 
@@ -39,19 +31,19 @@ class MLP:
             layers.append(np.random.rand(layer[1], prev_layer_size))
             prev_layer_size = layer[1]
 
-        printd("These are the layers: " + str(layers))
+        printd("These are the layers: " + str(layers), DEBUG)
         return layers
 
     def forwardPass(self, input_vec: np.ndarray) -> np.ndarray:
         currVal = input_vec
-        printd("curr_val start is: " + str(currVal))
+        printd("curr_val start is: " + str(currVal), DEBUG)
         for layerIdx in range(len(self.layers)):
             currActivationFunc = self.layer_schemas[layerIdx][0]
             currActivation = np.dot(self.layers[layerIdx], currVal)
-            printd("curr_val before activation func is: " + str(currActivation))
+            printd("curr_val before activation func is: " + str(currActivation), DEBUG)
             currVal = currActivationFunc(currActivation)
-            printd("curr_val after activation func is: " + str(currVal))
-        printd("Final curr_val is: " + str(currVal))
+            printd("curr_val after activation func is: " + str(currVal), DEBUG)
+        printd("Final curr_val is: " + str(currVal), DEBUG)
         return currVal
 
 
@@ -61,10 +53,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     DEBUG = args.debug
-    printd("debug mode is ON")
+    printd("debug mode is ON", DEBUG)
 
-    printd("Begin construct basic MLP")
-    layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
-                    (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
-    basicMLP = MLP(5, layerSchemas)
-    basicMLP.forwardPass(np.array([1, 2, 3, 4, 5]))
+    printd("Begin construct basic MLP", DEBUG)
+    # layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
+    #                 (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
+    # basicMLP = MLP(5, layerSchemas)
+    # basicMLP.forwardPass(np.array([1, 2, 3, 4, 5]))
+
+    # Simple binary classifier for oranges and apples
+    training_data = [(np.array([0.7, 0.8]), 1),
+                     (np.array([0.2, 0.5]), 0),
+                     (np.array([0.9, 0.7]), 1),
+                     (np.array([0.4, 0.3]), 0),
+                     (np.array([0.6, 0.6]), 1),
+                     (np.array([0.3, 0.4]), 0)
+                     ]
+
+    layerSchemas = [(ActivationFunction.LOGISTIC, 2), (ActivationFunction.TANH, 2),
+                    (ActivationFunction.RELU, 1)]
+    basicMLP = MLP(2, layerSchemas)
+
---
+++ src/Utils/DebugUtils.py
@@ -0,0 +1,4 @@
+def printd(string : str, debug: bool) -> None:
+    if debug:
+        print(string)
+
