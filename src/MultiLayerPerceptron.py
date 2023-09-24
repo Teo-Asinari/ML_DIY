93c79623a222840b5b910799a994e7d11279d772
Teo Asinari <tasinari@protonmail.com>
2023-09-23 19:28:13 -0700

    Adding python type hints and refactoring. Still very confused

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -1,6 +1,8 @@
 import numpy as np
 from enum import Enum
 
+from typing import List, Tuple
+
 
 class ActivationFunction(Enum):
     RELU = lambda x: np.maximum(0, x)
@@ -15,11 +17,11 @@ class MLP:
     output_layer_schema = (ActivationFunction.RELU, 5)
     layers = []
 
-    def __init__(self, input_size, layer_schemas):
+    def __init__(self, input_size: int, layer_schemas: List[Tuple[int, str]]):
         self.input_size = input_size
         self.layer_schemas = layer_schemas
 
-    def initLayers(self):
+    def initLayers(self) -> None:
         self.layers = []
         self.layers.append(np.random.rand(self.layer_schemas[0][1], self.input_size))
 
@@ -31,23 +33,23 @@ class MLP:
 
         print("These are the layers: " + str(self.layers))
 
-    def forwardPass(self, input_vec):
-         currVal = input_vec
-         print("curr_val start is: " + str(currVal))
-         for layerIdx in range(len(self.layers)):
-             currActivationFunc = self.layer_schemas[layerIdx][0]
-             currActivation = np.dot(self.layers[layerIdx], currVal)
-             print("curr_val before activation func is: " + str(currActivation))
-             currVal = currActivationFunc(currActivation)
-             print("curr_val after activation func is: " + str(currVal))
-         print("Final curr_val is: " + str(currVal))
-         return currVal
+    def forwardPass(self, input_vec: np.ndarray) -> np.ndarray:
+        currVal = input_vec
+        print("curr_val start is: " + str(currVal))
+        for layerIdx in range(len(self.layers)):
+            currActivationFunc = self.layer_schemas[layerIdx][0]
+            currActivation = np.dot(self.layers[layerIdx], currVal)
+            print("curr_val before activation func is: " + str(currActivation))
+            currVal = currActivationFunc(currActivation)
+            print("curr_val after activation func is: " + str(currVal))
+        print("Final curr_val is: " + str(currVal))
+        return currVal
 
 
 if __name__ == '__main__':
     print("Begin construct basic MLP")
     layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
-                          (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
+                    (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
     basicMLP = MLP(5, layerSchemas)
     basicMLP.initLayers()
-    basicMLP.forwardPass(np.array([1,2,3,4,5]))
+    basicMLP.forwardPass(np.array([1, 2, 3, 4, 5]))
