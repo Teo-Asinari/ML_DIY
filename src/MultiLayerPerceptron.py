ff9d9892e628c5b56b778bae06a223e8ee2b8cf8
Teo Asinari <tasinari@protonmail.com>
2023-09-24 11:23:34 -0700

    More refactoring, fixing type hints, adding debug log switch

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -1,13 +1,22 @@
+import argparse
 import numpy as np
 from enum import Enum
 
-from typing import List, Tuple
-
+from typing import List, Tuple, Callable
 
 class ActivationFunction(Enum):
     RELU = lambda x: np.maximum(0, x)
     TANH = lambda x: np.tanh(x)
     LOGISTIC = lambda x: 1 / (1 + np.exp(-x))
+
+activationFunc = Callable[[np.ndarray], np.ndarray]
+layerSchema = Tuple[activationFunc, int]
+
+DEBUG = False
+
+def printd(string : str) -> None:
+    if DEBUG:
+        print(string)
 
 
 class MLP:
@@ -15,39 +24,47 @@ class MLP:
     layer_schemas = []
     layers = []
 
-    def __init__(self, input_size: int, layer_schemas: List[Tuple[int, str]]):
+    def __init__(self, input_size: int, layer_schemas: List[layerSchema]):
         self.input_size = input_size
         self.layer_schemas = layer_schemas
+        self.layers = self.initLayers()
 
-    def initLayers(self) -> None:
-        self.layers = []
-        self.layers.append(np.random.rand(self.layer_schemas[0][1], self.input_size))
+    def initLayers(self) -> List[np.ndarray]:
+        layers = []
+        layers.append(np.random.rand(self.layer_schemas[0][1], self.input_size))
 
         prev_layer_size = self.layer_schemas[0][1]
 
         for layer in self.layer_schemas[1:]:
-            self.layers.append(np.random.rand(layer[1], prev_layer_size))
+            layers.append(np.random.rand(layer[1], prev_layer_size))
             prev_layer_size = layer[1]
 
-        print("These are the layers: " + str(self.layers))
+        printd("These are the layers: " + str(layers))
+        return layers
 
     def forwardPass(self, input_vec: np.ndarray) -> np.ndarray:
         currVal = input_vec
-        print("curr_val start is: " + str(currVal))
+        printd("curr_val start is: " + str(currVal))
         for layerIdx in range(len(self.layers)):
             currActivationFunc = self.layer_schemas[layerIdx][0]
             currActivation = np.dot(self.layers[layerIdx], currVal)
-            print("curr_val before activation func is: " + str(currActivation))
+            printd("curr_val before activation func is: " + str(currActivation))
             currVal = currActivationFunc(currActivation)
-            print("curr_val after activation func is: " + str(currVal))
-        print("Final curr_val is: " + str(currVal))
+            printd("curr_val after activation func is: " + str(currVal))
+        printd("Final curr_val is: " + str(currVal))
         return currVal
 
 
 if __name__ == '__main__':
-    print("Begin construct basic MLP")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
+
+    DEBUG = args.debug
+    printd("debug mode is ON")
+
+    printd("Begin construct basic MLP")
     layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
                     (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
     basicMLP = MLP(5, layerSchemas)
-    basicMLP.initLayers()
     basicMLP.forwardPass(np.array([1, 2, 3, 4, 5]))
