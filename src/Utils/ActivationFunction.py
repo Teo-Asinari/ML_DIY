b14552a9a3272b75a9444be37848a68e6a721892
Teo Asinari <tasinari@8x8.com>
2023-10-03 11:13:32 -0700

    Adding ceiling function for classification

--- src/Utils/ActivationFunction.py
+++ src/Utils/ActivationFunction.py
@@ -2,6 +2,7 @@ from enum import Enum
 from typing import Callable
 
 import numpy as np
+import math
 
 
 activationFunc = Callable[[np.ndarray], np.ndarray]
@@ -10,3 +11,4 @@ class ActivationFunction(Enum):
     RELU = lambda x: np.maximum(0, x)
     TANH = lambda x: np.tanh(x)
     SIGMOID = lambda x: 1 / (1 + np.exp(-x))
+    CEIL = lambda x: math.ceil(x)
