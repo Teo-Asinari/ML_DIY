f5dc19ea5c9fd2c53f869fe216443b7a16977dcc
Teo Asinari <tasinari@protonmail.com>
2023-11-04 13:02:14 -0700

    Making more progress on refactor. Running into issue of ValueError: shapes (2,1) and (2,) not aligned: 1 (dim 1) != 2 (dim 0), which I will need to fix tomorrow

--- src/MultiLayerPerceptron.py
+++ src/MultiLayerPerceptron.py
@@ -1,14 +1,12 @@
 import argparse
-import math
 
 import numpy as np
 
 from Utils.DebugUtils import printd
 from Utils.ActivationFunction import activationFunc, ActivationFunction
 
-from typing import List, Tuple
+from typing import List
 
-layerSchema = Tuple[activationFunc, int]
 
 DEBUG = False
 class MLPLayer:
@@ -17,47 +15,35 @@ class MLPLayer:
         self.inputsize = inputsize
         self.size = size
         self.func = func
-        self.weights = None
+        self.weights = np.random.rand(size, inputsize)
         self.activation = None
         self.output = None
+
+    def __repr__(self):
+        return f"MLPLayer(inputsize={self.inputsize}, " \
+               f"size={self.size}, " \
+               f"func={self.func}, " \
+               f"weights={self.weights.tolist()}, " \
+               f"activation={self.activation}, " \
+               f"output={self.output})"
 
 class MatrixOutMLPLayer(MLPLayer):
     def __init__(self, inputsize: int = 2, size: int = 2, func: activationFunc =
     ActivationFunction.RELU):
-        super.__init__(self, inputsize, size, func)
-        self.weights = np.random.rand(size, inputsize)
-        self.activation = np.zeros(size, 1)
-        self.output = np.zeros(size, 1)
+        super(MatrixOutMLPLayer, self).__init__(inputsize, size, func)
+        self.activation = np.zeros((size, 1))
+        self.output = np.zeros((size, 1))
 
 class ScalarOutMLPLayer(MLPLayer):
     def __init__(self, inputsize : int = 2, size: int = 2, func: activationFunc = ActivationFunction.RELU):
-        super.__init__(self, inputsize, size, func)
-        self.weights = np.random.rand(size, inputsize)
+        super(ScalarOutMLPLayer, self).__init__(inputsize, size, func)
         self.activation = 0
         self.output = 0
 
 class MLP:
-    def __init__(self, layers: List[MLPLayer]):
+    def __init__(self, layers: List[MLPLayer] = [], learning_rate: float  = 0):
         self.layers = layers
-        self.learning_rate = 0.1
-
-    # # Design: Each Layer is a list of [weights matrix,
-    # # last activation, activation_func(last activation)]
-    # def initLayers(self) -> List[np.ndarray]:
-    #     layers = [[np.random.rand(self.layer_schemas[0][1], self.input_size),
-    #                0 if self.layer_schemas[0][1] == 1 else np.zeros(self.layer_schemas[0][1], 1),
-    #                0 if self.layer_schemas[0][1] == 1 else np.zeros(self.layer_schemas[0][1], 1)]]
-    #
-    #     prev_layer_size = self.layer_schemas[0][1]
-    #
-    #     for layerSchema in self.layer_schemas[1:]:
-    #         layers.append([np.random.rand(layerSchema[1], prev_layer_size),
-    #                        0 if layerSchema[1] == 1 else np.zeros(layerSchema[1], 1),
-    #                        0 if layerSchema[1] == 1 else np.zeros(layerSchema[1], 1)])
-    #         prev_layer_size = layerSchema[1]
-    #
-    #     printd("These are the layers: " + str(layers), DEBUG)
-    #     return layers
+        self.learning_rate = learning_rate
 
     def forwardPass(self, input_vec: np.ndarray):
         currVal = input_vec
@@ -76,16 +62,18 @@ class MLP:
 # Hard-code the backprop matrices (derived by hand) for a small MLP of known layout
 class BasicMLP(MLP):
     def __init__(self):
-        self.input_size = 2
-        self.layer_schemas = [(ActivationFunction.RELU, 2), (ActivationFunction.RELU, 2),
-                    (ActivationFunction.SIGMOID, 1)]
-        self.layers = self.initLayers()
+        self.layers = [MatrixOutMLPLayer(2, 2, ActivationFunction.RELU),
+         MatrixOutMLPLayer(2, 2, ActivationFunction.RELU),
+         ScalarOutMLPLayer(1, 2, ActivationFunction.SIGMOID)]
+        self.learning_rate = 0.1
 
     def backPropSingleError(self, datum):
        input = datum[0]
        expected = datum[1]
        actual = self.forwardPass(input)
-       error = 0.5 * math.pow((expected - actual), 2)
+       error = 0.5 * pow(expected - actual, 2)
+
+       printd("Starting backprop. Error is " + str(error), DEBUG)
 
        dError = 1
        dOutput3 = (expected - actual)
@@ -98,9 +86,9 @@ class BasicMLP(MLP):
        dLayer1 = np.multiply(dOutput1, ActivationFunction.RELU_DERIV(self.layers[0].activation))
        dW1 = np.matmul(dLayer1, np.transpose(input))
 
-       self.layers[2][0] -= self.learning_rate * dW3
-       self.layers[1][0] -= self.learning_rate * dW2
-       self.layers[0][0] -= self.learning_rate * dW1
+       self.layers[2].weights -= self.learning_rate * dW3
+       self.layers[1].weights -= self.learning_rate * dW2
+       self.layers[0].weights -= self.learning_rate * dW1
 
 
 if __name__ == '__main__':
@@ -112,10 +100,6 @@ if __name__ == '__main__':
     printd("debug mode is ON", DEBUG)
 
     printd("Begin construct basic MLP", DEBUG)
-    # layerSchemas = [(ActivationFunction.LOGISTIC, 5), (ActivationFunction.TANH, 5),
-    #                 (ActivationFunction.TANH, 5), (ActivationFunction.RELU, 5)]
-    # basicMLP = MLP(5, layerSchemas)
-    # basicMLP.forwardPass(np.array([1, 2, 3, 4, 5]))
 
     # Simple binary classifier for oranges and apples
     training_data = [(np.array([0.7, 0.8]), 1),
@@ -133,3 +117,4 @@ if __name__ == '__main__':
     for datum in training_data:
         basicMLP.forwardPass(datum[0])
         basicMLP.backPropSingleError(datum)
+
