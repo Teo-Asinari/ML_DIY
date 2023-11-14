ea6f2759c5d69635b049e1202bb468de671cc3c7
Teo Asinari <tasinari@protonmail.com>
2023-11-14 11:59:12 -0800

    Making progress on reading iris2d and transforming it into the format used by my MLP

---
+++ src/sandbox/MultiLayerPerceptron/BasicMLP_Iris2D_Sandbox.py
@@ -0,0 +1,31 @@
+import argparse
+import os.path
+
+import pandas as pd
+
+
+from src.models.MultiLayerPerceptron import BasicMLP
+from src.utils.DebugUtils import printd
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
+    args = parser.parse_args()
+
+    DEBUG = args.debug
+    printd("debug mode is ON", DEBUG)
+
+    printd("Begin construct basic MLP", DEBUG)
+
+    relative_path = os.path.join('..', '..', '..', 'data', 'iris2d.csv')
+    df = pd.read_csv(relative_path)
+    binary_mapping = {'Setosa': 1, 'Versicolor' : 0}
+    df['variety'] = df['variety'].map(binary_mapping)
+    input_features = df.drop(columns=['variety'])
+
+    training_data = list(zip(input_features.to_numpy(), df['variety']))
+    # training_data = list(zip(input_features.apply(lambda x: x.to_numpy().reshape(-1, 1), axis=0).values, df['variety']))
+
+    basicMLP = BasicMLP()
+
+    basicMLP.trainingRun(training_data)
